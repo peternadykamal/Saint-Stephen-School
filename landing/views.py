e09290537c4d16f483a7489e6e27fcce8fa7d5e6
kamal nady <kamalnadykamal@gmail.com>
2023-07-30 20:06:34 +0300

    generated files

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 def landing(request):
-    context = {'title': 'Saint Stephen School'}
-    return render(request,'landing/index.html', context)
+  context = {'title': 'Saint Stephen School'}
+  return render(request, 'landing/index.html', context)
