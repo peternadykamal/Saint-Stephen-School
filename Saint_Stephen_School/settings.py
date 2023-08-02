de9b123ad22a4651b5a669f52b020ff9de7ae8bf
kamal nady <kamalnadykamal@gmail.com>
2023-08-02 22:16:59 +0300

    adding livereload to the settings file

--- Saint_Stephen_School/settings.py
+++ Saint_Stephen_School/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
 
     'landing.apps.LandingConfig',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'Saint_Stephen_School.urls'
