1421d61f79890257c3653754cbd7ce4ae515a527
kamal nady <kamalnadykamal@gmail.com>
2023-08-02 22:16:25 +0300

    check if the user loged in to send the profile name to the page

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,14 @@
+import profile
 from django.shortcuts import render
 
 
+from users import models
+
+
 def landing(request):
-  context = {'title': 'Saint Stephen School'}
+
+  context = {}
+  if request.user.is_authenticated:
+    models.Profile.getProfileName(request, context)
+
   return render(request, 'landing/index.html', context)
