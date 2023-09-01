b97312790b4a6784186d177f0e096d3c1deb4cdc
peter nady <nady.peter347@gmail.com>
2023-09-01 19:34:38 +0200

    migrations headache here is the problem i faced, i tried to add new permission in the 002 migration, and try to run migartion task, but no change happen, so what i understand is the following, you can think of migrations as a set of commits of changes, happend in the database models. you can move between migrations (commits) by calling the following command structure ``` python manage.py migrate users 0001_initial ``` by doing so i have to the 001 migration (002 is completely removed), basically mange.py start from calling reverse_code functions in decending order of the sequence of migrations, starting from 004 and up to 002.
    so if you want to add new permissions which is in 002 migration, you have to move back to 001 then add permissions you want to add then call the migration task,
    
    so this basically the new task do
    1. to move back to 001
    2. do the same as migrations task
    
    note that i faced another problem when trying to delete permissions which available in the database, because of the way i wrote the 002 reverse_code function, you have to make sure to have the permissions you want to delete in new_permissions array then run
    ```
    python manage.py migrate users 0001_initial
    ```
    after that remove permissions that you want to get delted from new_permissions array and run the migration task
    basically DON't USE "rest migrations for app_name" WHEN YOU WANT TO DELETE A PERMISSION, DO IT MANUALLY

--- users/migrations/0002_custom_permissions.py
+++ users/migrations/0002_custom_permissions.py
@@ -1,16 +1,12 @@
 import select
-from django.db import migrations
+
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-
+from django.db import migrations
 
 new_permissions = [
-    ('view_profile_talmza_level', 'view profile talmza level', 'users', 'profile'),
+    ('add_profile_talmza_level', 'add profile talmza level', 'users', 'profile'),
     ('change_profile_talmza_level', 'change profile talmza level', 'users', 'profile'),
-    ('view_profile_current_talmza_level_year',
-     'view profile current talmza level', 'users', 'profile'),
-    ('change_profile_current_talmza_level_year',
-     'change profile current talmza level', 'users', 'profile'),
 ]
 
 
@@ -18,12 +14,9 @@ def create_permissions(apps, schema_editor):
   # Create new permissions if they don't exist
   for i in range(len(new_permissions)):
     try:
-      if (len(new_permissions[i]) == 4):
-        codename, description, app, model = new_permissions[i]
-        selectedModel = apps.get_model(app, model)
-        content_type = ContentType.objects.get_for_model(selectedModel)
-      else:
-        content_type = None
+      codename, description, app, model = new_permissions[i]
+      selectedModel = apps.get_model(app, model)
+      content_type = ContentType.objects.get_for_model(selectedModel)
 
       Permission.objects.get_or_create(
           codename=codename, name=description,
@@ -35,7 +28,7 @@ def create_permissions(apps, schema_editor):
 def revert_create_permissions(apps, schema_editor):
   # Delete the created permissions (if you want to undo the changes during rollback)
   Permission.objects.filter(
-      codename__in=[codename for codename, _ in new_permissions]).delete()
+      codename__in=[codename for codename, _, _, _ in new_permissions]).delete()
 
 
 class Migration(migrations.Migration):
